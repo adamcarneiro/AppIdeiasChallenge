fa723367cafde9364f199c624e7b0b8d73106a64
Adamastor Chimalange <adamchimaj@gmail.com>
2020-10-01 15:09:56 +0100

    Criacao da pasta calculadora

---
+++ Basic/Calculator/calculator.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Calculator</title>
+</head>
+<body>
+    <div className="main">
+        <div className="screen-display">
+            <div className="screen-display" id="display-up"></div> 
+            <div className="screen-display" id="display-down"></div>
+        </div>
+        <div className="numrerical-area">
+            
+        </div>
+    </div>
+</body>
+</html>
